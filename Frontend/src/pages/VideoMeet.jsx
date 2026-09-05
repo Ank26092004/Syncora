eb7ef2bac7d991f9c119c4ba3463e335f0da691e
Ank26092004 <ankushlodhi26092004@gmail.com>
2026-09-05 23:42:00 +0530

    Enhance peerConfigConnections with TURN servers
    Added TURN server configurations to peer connection settings for improved connectivity.

--- Frontend/src/pages/VideoMeet.jsx
+++ Frontend/src/pages/VideoMeet.jsx
@@ -33,7 +33,27 @@ const server_url = server;
 var connections = {};
 
 const peerConfigConnections = {
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  // iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+     const peerConfigConnections = {
+     iceServers: [
+       { urls: "stun:stun.l.google.com:19302" },
+       {
+         urls: "turn:openrelay.metered.ca:80",
+         username: "openrelayproject",
+         credential: "openrelayproject",
+       },
+       {
+         urls: "turn:openrelay.metered.ca:443",
+         username: "openrelayproject",
+         credential: "openrelayproject",
+       },
+       {
+         urls: "turn:openrelay.metered.ca:443?transport=tcp",
+         username: "openrelayproject",
+         credential: "openrelayproject",
+       },
+     ],
+   };
 };
 
 export default function VideoMeetComponent() {
